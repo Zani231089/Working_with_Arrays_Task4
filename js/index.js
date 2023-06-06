7c84d051fccd0ff075ace6473e0bff52b1df3937
Zani231089 <231089@virtualwindow.co.za>
2023-06-06 19:17:29 +0200

    Final css

---
+++ js/index.js
@@ -0,0 +1,13 @@
+let vehicleInventory = []
+
+logVehicle = () => {
+
+    let type = document.getElementById("type").value;
+    let make = document.getElementById("make").value;
+    let model = document.getElementById("model").value;
+    let registration = document.getElementById("registration").value;
+    let condition = document.getElementById("condition").value;
+
+    if(){}
+}
+
